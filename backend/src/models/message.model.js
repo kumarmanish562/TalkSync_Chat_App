df191a81672827f26eb63f3ff2ee1a5cf6370d86
kumarmanish562 <qmanishkumar562@gmail.com>
2025-02-22 08:14:01 +0530

    changes in code

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    senderId:{
+    senderId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
@@ -14,17 +14,17 @@ const messageSchema = new mongoose.Schema(
     },
     text: {
       type: String,
-    
     },
     image: {
       type: String,
     },
-  
-},
-{
-  timestamps: true,
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("Message", messageSchema);
+// Define the Message model correctly
+const Message = mongoose.model("Message", messageSchema);
 
-export default Message;
+export default Message; // Export Message instead of undefined variable
