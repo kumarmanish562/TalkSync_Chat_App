761370b8b77e3876c6ed083dc7b467ca59a79520
kumarmanish562 <qmanishkumar562@gmail.com>
2025-02-22 08:15:11 +0530

    changes in routes

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-
+import { getUsersForSidebar, getMessages, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
-
-router.post("/:id", protectRoute, sendMessage);
+router.get('/messages/:id', protectRoute, getMessages);
+router.post('/messages/:id', protectRoute, sendMessage);
 
 export default router;
